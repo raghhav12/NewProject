32cca8addd21b31870cb8ef53073b20d32f1cf61
Sehgal <raghhavsehgal12@gmail.com>
2022-01-08 13:28:48 +0530

    initial commit

---
+++ getminmaxArray.c
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+// Initial Template for C
+
+#include <stdio.h>
+
+struct pair {
+    long long int min;
+    long long int max;
+};
+
+struct pair getMinMax(long long int arr[], long long int n) ;
+
+int main() {
+    long long int t, n, a[100002], i;
+    struct pair minmax;
+
+    scanf("%lld", &t);
+    while (t--) {
+        scanf("%lld", &n);
+
+        for (i = 0; i < n; i++) scanf("%lld", &a[i]);
+        minmax = getMinMax(a, n);
+        printf("%lld %lld\n", minmax.min, minmax.max);
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+// User function Template for C
+
+struct pair getMinMax(long long int arr[], long long int n) {
+    //my code
+    int min = 19998989,max = -666;
+    
+    struct pair New;
+    New.min = 0;
+    New.max = 0;
+    
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max = arr[i];
+        }
+        
+        
+        if(arr[i]<min){
+            min = arr[i];
+        }
+        
+        
+        
+    }
+    New.min = min;
+    New.max = max;
+    
+    return New;
+    
+    
+    
+    
+    
+}
